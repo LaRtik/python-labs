51d06256017906713f05d0aac5eb434b5949111f
Ilya Lazuk <i.lazuk45@gmail.com>
2022-03-30 09:49:14 +0300

    another code refactor

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import input
+import IO
 import parse
 import math_stats
 
 
 def main():
-    text = input.text_input()
+    text = IO.text_input()
     word_stats, sentences_stats = parse.parse_text(text)
-    input.word_stats_output(word_stats)
-    input.average_output(math_stats.average_words(sentences_stats))
-    input.median_output(math_stats.median_words(sentences_stats))
-    n, k = input.nk_input()
-    input.top_ngramms_output(math_stats.count_ngramms(word_stats, n, k), n, k)
+    IO.word_stats_output(word_stats)
+    IO.average_output(math_stats.average_words(sentences_stats))
+    IO.median_output(math_stats.median_words(sentences_stats))
+    n, k = IO.nk_input()
+    IO.top_ngramms_output(math_stats.count_ngramms(word_stats, n, k), n, k)
 
 
 if __name__ == '__main__':
