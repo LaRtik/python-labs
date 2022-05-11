35d0655252241c37030637e0b43af480d7d1731d
Ilya Lazuk <i.lazuk45@gmail.com>
2022-05-11 11:40:51 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="serializer",
+    version="0.5",
+    packages=["Serializers"],
+    url="https://github.com/LaRtik/python-labs/tree/lab2",
+    license="",
+    author="Ilya Lazuk",
+    author_email="i.lazuk45@gmail.com",
+    description="Serializer",
+)
